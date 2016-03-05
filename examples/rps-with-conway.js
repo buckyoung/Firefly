34c16f1adb9927de28766e095925b53704c278e2
Buck Young <buckcyoung@gmail.com>
2016-03-04 23:54:06 -0500

    Update example files, get them off root scope

---
+++ examples/rps-with-conway.js
@@ -0,0 +1,87 @@
+var FFExamples = FFExamples || {};
+
+FFExamples.rpsWithConway = {};
+
+FFExamples.rpsWithConway.initialize = function() {
+    var colors = [
+        'a', 
+        'b', 
+        'c'
+    ];
+    
+
+    Firefly(function(FF) {
+        var index = 0;
+
+        FF.registerState(colors[index++], [223, 163, 163], process);
+        FF.registerState(colors[index++], [171, 223, 163], process);
+        FF.registerState(colors[index++], [185, 163, 223], process);
+
+        FF.registerState('alive', [30, 30, 140], processAlive);
+
+        FF.initialize(initializeWorld(FF));
+    });
+
+    var rotator = 0;
+
+    function process(currentCell, nextCell) {
+        // Conway:
+        var aliveNeighborCount = currentCell.mooreNeighbors('alive');
+
+        if (aliveNeighborCount === 3) {
+            nextCell.setState('alive');
+            return;
+        }
+
+        // RPS:
+        var currentState = currentCell.getState();
+
+        var index = colors.indexOf(currentState);
+
+        index = (index + 1) % colors.length;
+
+        var newState = colors[index];
+
+        var neighborCount = currentCell.mooreNeighbors(newState);
+
+        if (neighborCount >= 3) {
+            nextCell.setState(newState);
+            return;
+        }
+        
+        nextCell.setState(currentState);
+    }
+
+    // Conway:
+    function processAlive(currentCell, nextCell) {
+        var aliveNeighborCount = currentCell.mooreNeighbors('alive');
+
+        var rando = Math.floor(Math.random() * 100);
+        var state = colors[rotator + rando  % colors.length];
+
+        if (aliveNeighborCount < 2 || aliveNeighborCount > 3) {
+            nextCell.setState(state);
+            return;
+        }
+
+        nextCell.setState('alive');
+    }
+
+    function initializeWorld(FF) {
+        return function(world, width, height) {
+            for (var i = 0; i < width; i++) {
+                for (var j = 0; j < height; j++) {
+                    var rando = Math.floor(Math.random() * 100);
+                    var state = colors[rotator + rando  % colors.length];
+
+                    // Conway:
+                    if (rando < 30) {
+                        state = 'alive';
+                    }
+
+                    world[i][j] = new FF.Cell(state, i, j); 
+                }
+            }
+        };
+    }
+};
